4adb4f532b3a5067ec3573d33d8ff2ad69a1426a
Ian <sebastianvaz123@hotmail.com>
2021-10-02 01:42:25 -0500

    Se hicieron mejoras en las instrucciones del test y se agregaron espacios para una mejor experiencia con el script
    Signed-off-by: Ian <sebastianvaz123@hotmail.com>

--- factorial.py
+++ factorial.py
@@ -12,12 +12,7 @@ def factorial_memo(input_value):
         factorial_dict[input_value] = input_value * factorial_memo(input_value-1)
     return factorial_dict[input_value]
 
-def Factorial(num_factorial):
-    #num_factorial=int(input("Ingrese el numero factorial que quiere calcular"))
-    #resultado_fact = factorial_memo(num_factorial)
-    #print(f"El numero {num_factorial} tiene un factorial de {resultado_fact} ")
-
-        
+def Factorial(num_factorial):        
     for i in range(1, num_factorial):
         resultado=factorial_memo(i)
 
--- main.py
+++ main.py
@@ -14,12 +14,13 @@ def main():
         initial_text = "\n\nBienvenido a los ejercicios del GBM Challenge 😄✌️ \n por favor ingrese el numero del test que desea iniciar \n "
         menu_test = "1: Calcula el Factorial de un numero 🤓\n 2: Buscar 🔍 si un array contiene ciertos elementos \n 3: Hacer la sumatoria ➕ de una lista predefinida\n "
         menu_test_2 = "4: Calcula el Modulo de dos numeros \n 5: Calcula los dias en un mes 📆\n "
-        exit_text = "\nSi quiere salir del menu simplemente presione ENTER"
-        test_number = input(initial_text + exit_text + menu_test+menu_test_2)
+        exit_text = "\nSi quiere salir del menu simplemente presione ENTER\n"
+
+        test_number = input(initial_text + exit_text + menu_test+menu_test_2+"Ingresa el numero del test:")
         if test_number == '1':
-            num_factorial=int(
+            num_factorial = int(
                 input("Ingrese el numero factorial que quiere calcular "))
-            resultado_fact=Factorial(num_factorial)
+            resultado_fact = Factorial(num_factorial)
             print(
                 f"El numero {num_factorial} tiene un factorial de {resultado_fact} ")
         elif test_number == '2':
@@ -27,16 +28,16 @@ def main():
         elif test_number == '3':
             suma()
         elif test_number == '4':
-            a=int(
+            a = int(
                 input("Ingrese los dos numeros a los cuales se le extraera el modulo: "))
-            b=int(input("Ingrese el dividendo:"))
+            b = int(input("Ingrese el dividendo:"))
             module(a, b)
         elif test_number == '5':
-            year=int(
-                input("Ingrese el año con el que quiere hacer el calculo "))
-            month=int(
-                input("Ingrese el numero del mes del que desea obtener los dias "))
-            print(numberOfDays(year, month))
+            year = int(
+                input("Ingrese el año con el que quiere hacer el calculo \n "))
+            month = int(
+                input("Ingrese el numero del mes del que desea obtener los dias\n "))
+            print(f"\n El numero de dias que tiene el mes numero {month} en el año {year} son {numberOfDays(year, month)} dias")
 
 
 if __name__ == "__main__":
